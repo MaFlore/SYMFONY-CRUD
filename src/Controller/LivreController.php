38c8a8e8041da7152290fb4d696076dd682315dc
AHADJITSE Yawo Florent Mathis <yawo.ahadjitse@ipnetinstitute.com>
2023-02-18 21:00:03 +0000

    Revues et apports de modification au fichier listLivre.html.twig

--- src/Controller/LivreController.php
+++ src/Controller/LivreController.php
@@ -8,87 +8,56 @@ use App\Repository\LivreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LivreController extends AbstractController
 {
 
-    #[Route("/",name: "allLivre")]
-
-     public function allLivre(LivreRepository $livreRepository){
-         $livres=$livreRepository->findAll();
-         return $this->render('livre/listLivre.html.twig',array(
-
-           'livres'=>$livres
-
-         ));
-
+    #[Route("/",name: "app_livres")]
+    public function livres(LivreRepository $livreRepository){
+        $livres=$livreRepository->findAll();
+        return $this->render('livre/listLivre.html.twig',array(
+            'livres'=>$livres
+        ));
      }
 
-    #[Route('/add', name: 'add')]
-
-
-    public function addLivre(Request $request, EntityManagerInterface $em)
+    #[Route('/ajouter', name: 'app_ajouter_livre')]
+    public function ajouterLivre(Request $request, EntityManagerInterface $em)
     {
-        $livre=new Livre();
-       
-        
+        $livre=new Livre(); 
         $form=$this->createForm(LivreType::class, $livre);
-
         $form->handleRequest($request);
-        if($form->isSubmitted() && $form->isValid()){
-    
-           $em ->persist($livre);
-           $em ->flush();
-
-           return $this->redirectToRoute('allLivre');
-  
+        if($form->isSubmitted() && $form->isValid()){ 
+            $em ->persist($livre);
+            $em ->flush();
+            return $this->redirectToRoute('app_livres');
         }
-
         return $this->render('livre/addLivre.html.twig',array(
             'form'=>$form->createView()
-
         ));
-
-        
     }
 
 
-    #[Route("/update/{id<\d+>}", name: "update")]
-
-    public function updateLivre(Request $request, Livre $livre, EntityManagerInterface $em)
+    #[Route("/modifier/{id<\d+>}", name: "app_modifier_livre")]
+    public function modifierLivre(Request $request, Livre $livre, EntityManagerInterface $em)
     {
-       
-        
         $form=$this->createForm(LivreType::class, $livre);
-
         $form->handleRequest($request);
         if($form->isSubmitted() && $form->isValid()){
-           $em->flush();
-
-           return $this->redirectToRoute('allLivre');
-  
+            $em->flush();
+            return $this->redirectToRoute('app_livres');
         }
-
         return $this->render('livre/updateLivre.html.twig',array(
             'form'=>$form->createView()
-
         ));
-
-        
     }
 
-    #[Route("/delete/{id<\d+>}")]
-
-   public function deleteLivre(Request $request, Livre $livre, EntityManagerInterface $em)
-   {
-          $em ->remove($livre);
-          $em ->flush();
-
-          return $this->redirectToRoute('allLivre');
-
-       
-   }
+    #[Route("/supprimer/{id<\d+>}", name: "app_suprimer_livre")]
+    public function supprimerLivre(Livre $livre, EntityManagerInterface $em)
+    {
+        $em ->remove($livre);
+        $em ->flush();
+        return $this->redirectToRoute('app_livres'); 
+    }
 
 }
